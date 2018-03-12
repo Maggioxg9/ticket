361c9ba1b383b92c4758965d4ae53cafd4183d26
Maggioxg9 <maggioxg9@aol.com>
2018-03-12 16:38:21 -0500

    push

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+	define("CLIENT_ID", "3MVG9d3kx8wbPieHPn2sTuBe8hJYy2FLwisUHzR7b5tjzTGUUpSFgqZuiV0MvKhxHDuRYdp0nwYUivamoYf8E");
+	define("CLIENT_SECRET", "5840737364626582501");
+	define("REDIRECT_URI", "https://34.205.27.179/oauth_callback.php");
+	define("LOGIN_URI", "https://test.salesforce.com");
+?>

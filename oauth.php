3c89d6f320190452b650c32153d39f29d7575b85
Maggioxg9 <maggioxg9@aol.com>
2018-03-12 17:23:09 -0500

    push

--- oauth.php
+++ oauth.php
@@ -5,6 +5,10 @@ $auth_url = LOGIN_URI
         . "/services/oauth2/authorize?response_type=code&client_id="
 
         . CLIENT_ID . "&redirect_uri=" . urlencode(REDIRECT_URI);
+		
+		$_SESSION['name']= $_POST['name'];
+		$_SESSION['ticketnum']=$_POST['ticketnum'];
+		$_SESSION['phone']=$_POST['phone'];
 
 header('Location: ' . $auth_url);
 ?>
